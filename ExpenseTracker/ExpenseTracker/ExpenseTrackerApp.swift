23058e9560ce5250c65eb1ff47bf52ea505c5373
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-01 18:03:05 +0200

    start new project file with xcode

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Ahmed Amin on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
